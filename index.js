7952728d94803f84e62e625c6a3408c566922318
Susan Gakii <susangakii71@gmail.com>
2025-08-29 13:29:08 +0300

    DOM Modifying Elements

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+// Remove Main
+main.remove();
+
+//Add new <h1> Element
+const newHeader= document.createElement("h1")
+
+//Add id
+newHeader.id="victory"
+
+//Add My Name
+newHeader.textContent = "Susan is the champion"
